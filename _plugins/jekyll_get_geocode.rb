7158d6b73d7746751c3eba04b13668fde5561c67
bk <bertrand.keller@gmail.com>
2016-10-20 14:16:47 +0200

    Update jekyll_geocode

--- _plugins/jekyll_get_geocode.rb
+++ _plugins/jekyll_get_geocode.rb
@@ -4,15 +4,22 @@ require 'yaml'
 require 'open-uri'
 require "i18n"
 
-module Jekyll_Get
+module Jekyll_Geocode
   class Generator < Jekyll::Generator
     safe true
     priority :highest
+
+    def request_service(url)
+      JSON.load(open(URI.encode(url)))
+    end
 
     def generate(site)
 
       #regEx for removing empty line
       regEx = /^[\s]*$\n/
+
+      #force locale
+      I18n.config.available_locales = :en
 
       #Get Geo service
       geo_service = 'https://nominatim.openstreetmap.org/?format=json&q='
@@ -45,58 +52,85 @@ module Jekyll_Get
       #Path
       path_yaml = "#{data_source}/#{outputfile}"
 
-      if File.file?(path_yaml) && outputfile
-        File.open(path_yaml, 'w') {|file| file.truncate(0) }
-      end
+      # if File.file?(path_yaml) && outputfile
+      #   File.open(path_yaml, 'w') {|file| file.truncate(0) }
+      # end
 
       # Load YML file
       members = YAML.load_file("#{data_source}/#{filename}")
 
       # Loop YML file
       members.each do |d|
-        # Test if a JSON file exists for performance issues
-        if !File.file?("#{data_source}/#{d[geo_name]}.json")
-          geo_name_field = I18n.transliterate(d[geo_name]).downcase.tr(" ", "-")
-          if d[geo_postcode]
-            geo_postcode_field = ",#{d[geo_postcode]}"
-          end
-          if d[geo_city]
-            geo_city_field = ",#{d[geo_city]}"
-          end
-          if d[geo_region]
-            geo_region_field = ",#{d[geo_region]}"
-          end
-          if d[geo_country]
-            geo_country_field = ",#{d[geo_country]}"
-          end
-          json = URI.encode("#{geo_service}#{d[geo_address]}#{geo_postcode_field}#{geo_city_field}#{geo_region_field}#{geo_country_field}&limit=1")
-          source = JSON.load(open(json))
+        geo_name_field = d[geo_name].downcase.tr(" ", "-")
+        if d[geo_postcode]
+          geo_postcode_field = ", #{d[geo_postcode]}"
+        end
+        if d[geo_city]
+          geo_city_field = ", #{d[geo_city]}"
+        end
+        if d[geo_region]
+          geo_region_field = ", #{d[geo_region]}"
+        end
+        if d[geo_country]
+          geo_country_field = ", #{d[geo_country]}"
+        end
+        geo_coord = "#{d[geo_address]}#{geo_postcode_field}#{geo_city_field}#{geo_region_field}#{geo_country_field}"
+        geo_request = "#{geo_service}#{geo_coord}&limit=1"
+        p geo_request
 
-          # Loop for an YML output
-          if outputfile
-            source.each do |coordinates|
-              data = [ "title" => "#{d[geo_name]}", "location" => { "latitude" => "#{coordinates["lat"]}","longitude" => "#{coordinates["lon"]}" } ]
-              data_yml = data.to_yaml.gsub("---", "").gsub(regEx, '')
-              # Test if there is any yaml files and create file
+
+        # Loop for an YML output
+        if outputfile
+
+          # No cache at the first loop
+          geo_cache = true
+
+          # Read the YML file
+          if File.file?(path_yaml)
+            file_yaml = YAML.load(File.open(path_yaml))
+          end
+
+          # If YML file test if and address are the same as in the source file
+          if file_yaml
+            file_yaml.each do |coordinates|
+              if coordinates['address'] == geo_coord
+                geo_cache = false
+              end
+            end
+          else
+            geo_cache = true
+          end
+
+          # Write data
+          if geo_cache == true
+            p "geocode is requesting #{geo_coord}"
+            request_service(geo_request).each do |coordinates|
+              data = [ "title" => "#{d[geo_name]}", "url" => "##{d[geo_name]}", "data_set" => "01", "location" => { "latitude" => "#{coordinates["lat"]}","longitude" => "#{coordinates["lon"]}" }, "address" => "#{geo_coord}" ]
+              data_yml = data.to_yaml
+              # Add data in the YML file
               if !File.file?(path_yaml)
                 File.open(path_yaml, "w") {|f| f.write(data_yml) }
               end
-              # Test if there is yaml files and add data recursively
               if File.file?(path_yaml)
                 File.open(path_yaml, 'a') { |f|
-                  f.puts data_yml
+                  f.puts data_yml.gsub("---", "").gsub(regEx, '')
                 }
+                # Load YML file => remove duplicated entries
+                file_yaml = YAML.load(File.open(path_yaml)).uniq { |s| s.first }
+                File.open(path_yaml, "w") {|f| f.write(file_yaml.to_yaml) }
               end
             end
-          # Loop for an JSON output
-          else
-            site.data[geo_name_field] = source
-            #Create a JSON  files if cache is enabled
-            if site.config['jekyll_geocode']['cache']
-              path_json = "#{data_source}/#{geo_name_field}.json"
-              open(path_json, 'wb') do |file|
-                file << JSON.generate(site.data[geo_name_field])
-              end
+          end
+        end
+
+        # JSON output :: Test if a JSON file exists for performance issues
+        if !outputfile && !File.file?("#{data_source}/#{d[geo_name]}.json")
+          site.data[geo_name_field] = request_service(geo_request)
+          #Create a JSON  files if cache is enabled
+          if site.config['jekyll_geocode']['cache']
+            path_json = "#{data_source}/#{geo_name_field}.json"
+            open(path_json, 'wb') do |file|
+              file << JSON.generate(site.data[geo_name_field])
             end
           end
         end
