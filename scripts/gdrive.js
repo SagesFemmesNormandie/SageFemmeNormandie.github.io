d1e1f319e57241e029f20de7754d907c3ea9d692
Bertrand Keller <bertrand.keller@gmail.com>
2021-09-22 14:26:35 +0200

    update rake file

--- scripts/gdrive.js
+++ scripts/gdrive.js
@@ -18,43 +18,44 @@ const transform = require('./transform');
 function getJson(id, gid) {
   fetch(`https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:json&tq&gid=${gid}`)
   .then(res => res.text())
-    .then(text => {
-      const json = JSON.parse(text.substr(47).slice(0, -2))
-      return json
-    })
-    .then(json => {
-      const data = []
-      const cols = json.table.cols
-      const rows = json.table.rows
-      for(const row of rows) {
-        const formattedRow = {}
-        const activites = []
-        const activitesdata = []
-        row.c.forEach(function (value, i) {
-          var key = slugify(cols[i].label.toLowerCase().trim())
-          key = key.includes('ttention') ? 'ville' : key;
-          if (value && key && value.v !== null && (value.v !== 1 && value.v !== 0)) {
-            formattedRow[key] = value.v;
-          }
-          if (value && key && value.v !== null && (value.v === 1 || value.v === 0)) {
-            activitesdata.push({ "nom": cols[i].label });
-          }
-        })
-        if (formattedRow['prenom']) {
-          formattedRow['nom_entier'] = transform.slugify(formattedRow['prenom'] + '-' + formattedRow['nom']).toLowerCase().trim();
-          formattedRow['activites'] = activitesdata;
-          data.push(formattedRow)
+  .then(text => {
+    const json = JSON.parse(text.substr(47).slice(0, -2))
+    return json
+  })
+  .then(json => {
+    const data = []
+    const cols = json.table.cols
+    const rows = json.table.rows
+    for(const row of rows) {
+      const formattedRow = {}
+      const activites = []
+      const activitesdata = []
+      row.c.forEach(function (value, i) {
+        var key = slugify(cols[i].label.toLowerCase().trim())
+        key = key.includes('ttention') ? 'ville' : key;
+        if (value && key && value.v !== null && (value.v !== 1 && value.v !== 0)) {
+          formattedRow[key] = value.v;
         }
+        if (value && key && value.v !== null && (value.v === 1 || value.v === 0)) {
+          activitesdata.push({ "nom": cols[i].label });
+        }
+      })
+      if (formattedRow['prenom']) {
+        formattedRow['nom_entier'] = transform.slugify(formattedRow['prenom'] + '-' + formattedRow['nom']).toLowerCase().trim();
+        formattedRow['activites'] = activitesdata;
+        data.push(formattedRow)
+      }
 
-      }
-      const dataYAML = YAML.stringify(data);
-      console.log(dataYAML)
-      if (!fs.existsSync('./_data/')) {
-        fs.mkdirSync('./_data/');
-        console.log('create : ./_data/')
-      }
-      fs.writeFileSync('./_data/members.yml', dataYAML, 'utf8');
-    })
+    }
+    const dataYAML = YAML.stringify(data);
+    console.log(dataYAML)
+    if (!fs.existsSync('./_data/')) {
+      fs.mkdirSync('./_data/');
+      console.log('create : ./_data/')
+    }
+    fs.writeFileSync('./_data/members.yml', dataYAML, 'utf8');
+    console.log('create : ./_data/members.yml')
+  })
 }
 
 getJson('1nt-D_VmZT4ex_7EwkXRlcj2jIJ4gafgm1mtuxkbhSts','1')
